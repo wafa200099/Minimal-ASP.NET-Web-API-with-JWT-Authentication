d7d5d3cd2d5f40768e0134a6741a16cd776b2478
wafaa alomour <wafaa.alomour@gmail.com>
2024-11-11 23:37:11 +0200

    Implementing JWT Authentication,Controller,Configuring JWT Authentication

---
+++ Minimal ASP.NET Web API with JWT Authentication/Controllers/WeatherController.cs
@@ -0,0 +1,16 @@
+﻿namespace Minimal_ASP.NET_Web_API_with_JWT_Authentication.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherController : ControllerBase
+{
+    [Authorize]
+    [HttpGet]
+    public IActionResult GetWeather()
+    {
+        return Ok("You are authorized to see this message!");
+    }
+}
